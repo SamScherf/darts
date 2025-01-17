d5427697e5921e6a2f9a10ed057c26b6792c378d
Sam <github@samscherf.com>
2025-01-16 22:53:06 -0500

    Fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 const port = 5000;
 
 app.get('/', (req, res) => {
