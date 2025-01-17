120f1e46a90ac2c5f87bc67ff5e7633414a2f622
Sam <github@samscherf.com>
2025-01-16 23:59:06 -0500

    Get started with blueprint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import axios from 'axios';
+import { Button, Card, InputGroup, Tooltip } from "@blueprintjs/core";
+import './App.css';
 
 export function App() {
 	const [message, setMessage] = React.useState<string>('');
+	const [showPassword, setShowPassword] = React.useState<Boolean>(false);
+
+	const handleLockClick = React.useCallback(() => setShowPassword(currentVal => !currentVal), [])
 
 	React.useEffect(() => {
 		axios.get('http://localhost:5000')
@@ -10,9 +15,29 @@ export function App() {
 		.catch(error => console.error('Error fetching data', error));
 	}, []);
 
+	const lockButton = (
+           <Tooltip content={`${showPassword ? "Hide" : "Show"} Password`}>
+               <Button
+                   icon={showPassword ? "unlock" : "lock"}
+				   intent="warning"
+                   minimal={true}
+                   onClick={handleLockClick}
+               />
+           </Tooltip>
+       );
+
 	return (
-		<div>
-			<h1>{message}</h1>
+		<div className="homepage">
+			<Card className="login-container">
+				<div>{message}</div>
+				<InputGroup
+					type={showPassword ? "text" : "password"}
+					placeholder="Enter your password..."
+					rightElement={lockButton}
+					className="password-field"
+				/>
+				<Button text="Login" intent="success" fill={true} />
+			</Card>
 		</div>
 	);
 }
