3645ab7ede929c5359f4d6560225ca2dadb083fc
Sam <github@samscherf.com>
2025-01-18 20:49:19 -0500

    Add more pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,21 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import './style.css';
 import { Login } from './pages/Login.tsx';
 import { Home } from './pages/Home.tsx';
+import { Stats } from './pages/Stats.tsx';
+import { Play } from './pages/Play.tsx';
 
 export function App() {
 	const [secret, setSecret] = React.useState<string | undefined>(undefined)
+
+	const handleLogout = React.useCallback(() => setSecret(undefined), [setSecret])
 
 	return (
 	<Router>
       <Routes>
         <Route path="/login" element={<Login setSecret={setSecret} />} />
-        <Route path="/" element={secret != null ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/" element={secret != null ? <Home onLogout={handleLogout} /> : <Navigate to="/login" />} />
+        <Route path="/Play" element={secret != null ? <Play /> : <Navigate to="/login" />} />
+        <Route path="/Stats" element={secret != null ? <Stats /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
 	);
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,28 @@
+import { Button } from '@blueprintjs/core';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const Home: React.FC= () => {
-  return (
+interface HomeProps {
+	onLogout: () => void;
+}
+export const Home: React.FC<HomeProps> = ({ onLogout }) => {
+	const navigate = useNavigate()
+
+	const handlePlayClick = React.useCallback(() => navigate("/play"), [navigate]);
+	const handleStatsClick = React.useCallback(() => navigate("/stats"), [navigate]);
+	const handleLogoutClick = React.useCallback(() => {
+		onLogout();
+	}, [onLogout]);
+
+  	return (
 		<div className="home">
 			<div className="logo-container">
-				<img src="/dwat-logo.png" className="dartboard" alt="dartboard" />
+				<img src="/dwat-logo.png" className="logo" alt="DWAT logo" />
+			</div>
+			<div className="buttons-container">
+				<Button text="Play" intent="success" large={true} fill={true} onClick={handlePlayClick} />
+				<Button text="Stats" intent="primary" large={true} fill={true} onClick={handleStatsClick} />
+				<Button text="Logout" intent="danger" large={true} fill={true} onClick={handleLogoutClick} />
 			</div>
 		</div>
   );
