8f4375c6ed0e0c2eaad2408025dc019b94808d30
Sam <github@samscherf.com>
2025-01-18 20:21:31 -0500

    Add logo

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export const Home: React.FC= () => {
   return (
 		<div className="home">
-			<div>Welcome</div>
+			<div className="logo-container">
+				<img src="/dwat-logo.png" className="dartboard" alt="dartboard" />
+			</div>
 		</div>
   );
 };
